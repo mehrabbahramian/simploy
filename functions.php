ff724a0323b9d0b87b70be46a9b440b3ce92a90f
Mehrab Bahrami <meh.raab77.brmn@gmail.com>
2024-09-20 15:30:22 +0330

    funcs

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+require_once get_template_directory() . '/inc/functions_title.php';
+require_once get_template_directory() . '/inc/functions_enqueue_styles.php';
+require_once get_template_directory() . '/inc/functions_menu.php';
+require_once get_template_directory() . '/inc/functions_enqueue_scripts.php';
